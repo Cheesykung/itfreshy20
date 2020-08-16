e345e81e0035f59e7ad8642c9ae4c4948bc569b9
Jokerbm <37797569+Jokerbm@users.noreply.github.com>
2020-08-16 18:30:13 +0700

    connect to firebae

--- functions/api/controller/testController.js
+++ functions/api/controller/testController.js
@@ -11,7 +11,7 @@ const User = require("./models/User");
 const axios = require("axios");
 const facebookStrategy = require("passport-facebook").Strategy;
 const firestore = admin.firestore();
-
+const { v4: uuidv4 } = require('uuid');
 const testController = express();
 
 testController.use(session({ secret: "ilovescotchscotfchyscotchscotch" }));
@@ -33,155 +33,189 @@ testController.get('/test', async (req, res) => {
             'statusText': 'Internal Server Error',
             'error': true
         });
-        return ;
+        return;
     }
 });
 
 
 passport.use(
     new facebookStrategy(
-      {
-        // pull in our app id and secret from our auth.js file
-        clientID: "306264320456438",
-        clientSecret: "f076de5e27c1ea459950049ccad236a1",
-        callbackURL: "http://localhost:8080/facebook/callback",
-        profileFields: ["id", "displayName", "name", "gender", "photos", "email"],
-      }, // facebook will send back the token and profile
-      function(token, refreshToken, profile, done) {
-        // asynchronous
-        process.nextTick(function() {
-          // find the user in the database based on their facebook id
-          User.findOne({ uid: profile.id }, function(err, user) {
-            // if there is an error, stop everything and return that
-            // ie an error connecting to the database
-            if (err) return done(err);
-  
-            // if the user is found, then log them in
-            if (user) {
-              console.log("user found");
-              console.log(user);
-              return done(null, user); // user found, return that user
-            } else {
-              // if there is no user found with that facebook id, create them
-              var newUser = new User();
-  
-              // set all of the facebook information in our user model
-              newUser.uid = profile.id; // set the users facebook id
-              newUser.token = token; // we will save the token that facebook provides to the user
-              newUser.name =
-                profile.name.givenName + " " + profile.name.familyName; // look at the passport user profile to see how names are returned
-              try {
-                newUser.email = profile.emails[0].value;
-              } catch (err) {
-                console.log("not haVE ");
-              }
-              newUser.gender = profile.gender;
-              newUser.pic =
-                "https://graph.facebook.com/" +
-                profile.id +
-                "/picture" +
-                "?type=large" +
-                "&access_token=" +
-                token;
-              newUser.point = 0;
-              // save our user to the database
-              newUser.save(function(err) {
-                if (err) throw err;
-  
-                // if successful, return the new user
-                return done(null, newUser);
-              });
-            }
-          });
-        });
-      }
+        {
+            // pull in our app id and secret from our auth.js file
+            clientID: "306264320456438",
+            clientSecret: "f076de5e27c1ea459950049ccad236a1",
+            callbackURL: "http://localhost:8080/facebook/callback",
+            profileFields: ["id", "displayName", "name", "gender", "photos", "email"],
+        }, // facebook will send back the token and profile
+        function (token, refreshToken, profile, done) {
+            // asynchronous
+            process.nextTick(async function () {
+                const citiesRef = db.collection('users');
+                const snapshot = await citiesRef.where('uid', '==', profile.id).get();
+                if (snapshot.empty) {
+                    console.log("new")
+                    const res = await db.collection('users').add({
+                        id: uuidv4(),
+                        name: profile.name.givenName + " " + profile.name.familyName,
+                        token: token,
+                        pic: "https://graph.facebook.com/" +
+                            profile.id +
+                            "/picture" +
+                            "?type=large" +
+                            "&access_token=" +
+                            token,
+                        point: 0,
+                        uid: profile.id
+                    })
+                    const snapshot = await citiesRef.where('uid', '==', profile.id).get();
+                    snapshot.forEach(doc => {
+                        console.log("old")
+                        console.log(doc.data())
+                        return done(null, doc.data());
+                    });
+                }
+                else {
+                    snapshot.forEach(doc => {
+                        console.log("old")
+                        console.log(doc.data())
+                        return done(null, doc.data());
+                    });
+                }
+
+                // find the user in the database based on their facebook id
+                // User.findOne({ uid: profile.id }, function (err, user) {
+                //     // if there is an error, stop everything and return that
+                //     // ie an error connecting to the database
+                //     if (err) return done(err);
+
+                //     // if the user is found, then log them in
+                //     if (user) {
+                //         console.log("user found");
+                //         console.log(user);
+                //         return done(null, user); // user found, return that user
+                //     } else {
+                //         // if there is no user found with that facebook id, create them
+                //         var newUser = new User();
+
+                //         // set all of the facebook information in our user model
+                //         newUser.uid = profile.id; // set the users facebook id
+                //         newUser.token = token; // we will save the token that facebook provides to the user
+                //         newUser.name =
+                //             profile.name.givenName + " " + profile.name.familyName; // look at the passport user profile to see how names are returned
+                //         try {
+                //             newUser.email = profile.emails[0].value;
+                //         } catch (err) {
+                //             console.log("not haVE ");
+                //         }
+                //         newUser.gender = profile.gender;
+                //         newUser.pic =
+                //             "https://graph.facebook.com/" +
+                //             profile.id +
+                //             "/picture" +
+                //             "?type=large" +
+                //             "&access_token=" +
+                //             token;
+                //         newUser.point = 0;
+                //         // save our user to the database
+                //         newUser.save(function (err) {
+                //             if (err) throw err;
+
+                //             // if successful, return the new user
+                //             return done(null, newUser);
+                //         });
+                //     }
+                // });
+            });
+        }
     )
-  );
-  
-  passport.serializeUser(function(user, done) {
+);
+
+passport.serializeUser(function (user, done) {
     done(null, user.id);
-  });
-  
-  // used to deserialize the user
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
+});
+
+// used to deserialize the user
+passport.deserializeUser(async function (id, done) {
+    const citiesRef = db.collection('users');
+    const snapshot = await citiesRef.where('id', '==', id).get();
+    snapshot.forEach(doc => {
+        console.log(doc.id, '=>', doc.data());
+        done(null, doc.data())
     });
-  });
-  
-  testController.get("/account", isLoggedIn, function(req, res) {
+});
+
+testController.get("/account", isLoggedIn, function (req, res) {
     console.log(req.user);
     // res.render('profile', {
     //     user: req.user // get the user out of session and pass to template
     // });
-  });
-  
-  testController.get("/control", isLoggedIn, function(req, res) {
+});
+
+testController.get("/control", isLoggedIn, function (req, res) {
     console.log(req.user.role);
-  });
-  
-  testController.get("/genqrcode", isLoggedIn, function(req, res) {
+});
+
+testController.get("/genqrcode", isLoggedIn, function (req, res) {
     console.log("genqr" + req.user.name);
     var textfirst = CryptoJS.AES.encrypt(req.user.uid, "secret key 123");
     var ciphertext = encodeURI(textfirst);
     console.log(ciphertext);
     res.render("qrcode", {
-      user: req.user,
-      text: "localhost:5000/qrcode/" + ciphertext, // get the user out of session and pass to template
+        user: req.user,
+        text: "localhost:5000/qrcode/" + ciphertext, // get the user out of session and pass to template
     });
-  });
-  testController.get("/qrcode/:id", function(req, res) {
+});
+testController.get("/qrcode/:id", function (req, res) {
     var dec = decodeURI(req.params.id);
     var bytes = CryptoJS.AES.decrypt(dec.toString(), "secret key 123");
     var plaintext = bytes.toString(CryptoJS.enc.Utf8);
     console.log(typeof plaintext);
     if (plaintext == "") {
-      res.send("ไม่ผ่าน");
+        res.send("ไม่ผ่าน");
     } else {
-      User.findOne({ uid: plaintext }, function(err, obj) {
-        if (err) {
-          res.send("ไม่ผ่าน");
-        }
-        if (obj.uid != null) {
-          res.send("point + 1");
-        }
-      });
+        User.findOne({ uid: plaintext }, function (err, obj) {
+            if (err) {
+                res.send("ไม่ผ่าน");
+            }
+            if (obj.uid != null) {
+                res.send("point + 1");
+            }
+        });
     }
-  });
-  // route middleware to make sure
-  function isLoggedIn(req, res, next) {
+});
+// route middleware to make sure
+function isLoggedIn(req, res, next) {
     // if user is authenticated in the session, carry on
     if (req.isAuthenticated()) return next();
-  
+
     // if they aren't redirect them to the home page
     res.redirect("/");
-  }
-  testController.get(
+}
+testController.get(
     "/auth/facebook",
     passport.authenticate("facebook", { scope: "email" })
-  );
-  
-  testController.route("/facebook/callback").get(
+);
+
+testController.route("/facebook/callback").get(
     passport.authenticate("facebook", {
-      successRedirect: "/profile",
-      failureRedirect: "/",
+        successRedirect: "/profile",
+        failureRedirect: "/",
     })
-  );
-  
-  testController.get("/api/user", isLoggedIn, (req, res) => {
+);
+
+testController.get("/api/user", isLoggedIn, (req, res) => {
     if (isLoggedIn) res.json(req.user);
-  });
-  
-  testController.get("/", (req, res) => {
+});
+
+testController.get("/", (req, res) => {
     res.render("index");
-  });
-  testController.get("/a", (req, res) => {
-    // console.log(plaintext);
-    res.send("fortest");
-  });
-  testController.get("/logout", function(req, res) {
+});
+testController.get("/a", async (req, res) => {
+    res.send("start")
+});
+testController.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
-  });
-  
+});
+
 module.exports = testController;
