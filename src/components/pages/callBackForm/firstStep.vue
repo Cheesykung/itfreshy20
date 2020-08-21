026d27288ebb661849a42db841e96b13476cc040
Unity Dev <35415746+peetnawapol@users.noreply.github.com>
2020-08-21 16:10:01 +0700

    test

--- src/components/pages/callBackForm/firstStep.vue
+++ src/components/pages/callBackForm/firstStep.vue
@@ -2,36 +2,58 @@
   <pageHFull>
     <template #headline>
       STEP
-      <span class="text-blue-500 font-semibold">1</span>
-    <p class="text-sm">Profile</p>
+      <span class="text-orange-500 font-semibold">1</span>
+      <p class="text-sm">Profile</p>
     </template>
     <template #body>
       <!--- Form area --->
       <formContain>
         <template #content>
-          <span class="space-y-3 flex flex-col">
-          <p class="text-left text-gray-100 text-opacity-100">Name</p>
-          <input type="text" class="base-input rounded shadow leading-tight focus:outline-none focus:shadow-outline" v-model="name" />
-          </span>
-           <span class="space-y-3 flex flex-col">
-          <p class="text-left text-gray-100 text-opacity-100">Nickname</p>
-          <input type="text" class="base-input rounded shadow leading-tight focus:outline-none focus:shadow-outline" v-model="nickname" />
-           </span>
-          <span class="space-y-3 flex flex-col">
-          <p class="text-left text-gray-100 text-opacity-100">Age</p>
-          <input type="text" class="base-input rounded shadow leading-tight focus:outline-none focus:shadow-outline" v-model="age" />
-          </span>
-          <span class="content-center flex-wrap">
-            <button class="font-medium text-sm w-1/6 h-8 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 border-1 text-white rounded" type="button">
-              next
+          <div class="flex flex-col flex-wrap space-y-8">
+            <span class="space-y-3 flex flex-col">
+              <p class="text-left text-gray-100 text-opacity-100">Name</p>
+              <input
+                type="text"
+                class="base-input rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                v-model="name"
+              />
+            </span>
+            <span class="space-y-3 flex flex-col">
+              <p class="text-left text-gray-100 text-opacity-100">Nickname</p>
+              <input
+                type="text"
+                class="base-input rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                v-model="nickname"
+              />
+            </span>
+            <span class="space-y-3 flex flex-col">
+              <p class="text-left text-gray-100 text-opacity-100">Age</p>
+              <input
+                type="text"
+                class="base-input rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                v-model="age"
+              />
+            </span>
+          </div>
+          <!--- Start step zone --->
+          <div class="flex flex-col items-center justify-center space-y-10 text-gray-400 px-4">
+            <button
+              type="submit"
+              class="btn bg-blue-600 hover:bg-blue-800 text-blue-200 px-12 py-3 md:px-12 md:py-4 capitalize font-medium text-sm rounded-md flex items-center"
+              
+              id="sub_button"
+            >
+              next step
+              <ion-icon name="chevron-forward-outline"></ion-icon>
             </button>
-          </span>
-            <span class="flex flex-row flex-no-wrap space-x-3 items-center justify-center">
-            <span class="bullet"></span>
-            <span class="bullet active"></span>
-            <span class="bullet"></span>
-            <span class="bullet"></span>
-        </span> 
+            <span class="flex flex-row flex-no-wrap space-x-3">
+              <span class="bullet active"></span>
+              <span class="bullet"></span>
+              <span class="bullet"></span>
+              <span class="bullet"></span>
+            </span>
+          </div>
+          <!--- End step zone --->
         </template>
       </formContain>
       <!--- End of form area --->
@@ -49,15 +71,13 @@ export default {
     formContain
   },
   data() {
-      return {
-          name: "",
-          nickname: "",
-          age: null
-      }
+    return {
+      name: "",
+      nickname: "",
+      age: null
+    };
   },
-  methods: {
-
-  }
+  methods: {}
 };
 </script>
 
--- src/components/pages/callBackForm/secondStep.vue
+++ src/components/pages/callBackForm/secondStep.vue
@@ -3,44 +3,64 @@
     <template #headline>
       STEP
       <span class="text-orange-500 font-semibold">2</span>
-    <p class="text-sm">About you</p>
+      <p class="text-sm">About you</p>
     </template>
     <template #body>
       <!--- Form area --->
       <formContain>
         <template #content>
-            <span class="space-y-3 flex flex-col">
-                <p class="text-left text-gray-100 text-opacity-100">Branch : {{ branch }}</p>
-                <select class="base-input rounded shadow leading-tight focus:outline-none focus:shadow-outline" v-model="branch" >
-                    <option>IT</option>
-                    <option>DSBA</option>
-                    <option>BIT</option>
-                </select>
-            </span>
+          <div class="flex flex-col flex-wrap space-y-8">
           <span class="space-y-3 flex flex-col">
-                <p class="text-left text-gray-100 text-opacity-100">Year : {{ year }}</p>
-                <select  class="base-input rounded shadow leading-tight focus:outline-none focus:shadow-outline" v-model="year" >
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                </select>
-           </span>
-          <span class="space-y-3 flex flex-col">
-          <p class="text-left text-gray-100 text-opacity-100">Contact</p>
-          <input type="text" class="base-input rounded shadow leading-tight focus:outline-none focus:shadow-outline" v-model="contact" />
+            <p class="text-left text-gray-100 text-opacity-100">Branch : {{ branch }}</p>
+            <select
+              class="base-input rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              v-model="branch"
+            >
+              <option>IT</option>
+              <option>DSBA</option>
+              <option>BIT</option>
+            </select>
           </span>
-          <span class="content-center flex-wrap">
-            <button class="font-medium w-1/6 h-8 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 border-1 text-white rounded" type="button">
-              next
+          <span class="space-y-3 flex flex-col">
+            <p class="text-left text-gray-100 text-opacity-100">Year : {{ year }}</p>
+            <select
+              class="base-input rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              v-model="year"
+            >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+            </select>
+          </span>
+          <span class="space-y-3 flex flex-col">
+            <p class="text-left text-gray-100 text-opacity-100">Contact</p>
+            <input
+              type="text"
+              class="base-input rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              v-model="contact"
+            />
+          </span>
+          </div>
+          <!--- Start step zone --->
+          <div class="flex flex-col items-center justify-center space-y-10 text-gray-400 px-4">
+            <button
+              type="submit"
+              class="btn bg-blue-600 hover:bg-blue-800 text-blue-200 px-12 py-3 md:px-12 md:py-4 capitalize font-medium text-sm rounded-md flex items-center"
+              
+              id="sub_button"
+            >
+              next step
+              <ion-icon name="chevron-forward-outline"></ion-icon>
             </button>
-          </span>
-          <span class="flex flex-row flex-no-wrap space-x-3 justify-center">
-            <span class="bullet"></span>
-            <span class="bullet"></span>
-            <span class="bullet active"></span>
-            <span class="bullet"></span>
-          </span>
+            <span class="flex flex-row flex-no-wrap space-x-3">
+              <span class="bullet active"></span>
+              <span class="bullet"></span>
+              <span class="bullet"></span>
+              <span class="bullet"></span>
+            </span>
+          </div>
+          <!--- End step zone --->
         </template>
       </formContain>
       <!--- End of form area --->
@@ -58,15 +78,13 @@ export default {
     formContain
   },
   data() {
-      return {
-          branch: "",
-          year: "",
-          contact: null
-      }
+    return {
+      branch: "",
+      year: "",
+      contact: null
+    };
   },
-  methods: {
-
-  }
+  methods: {}
 };
 </script>
 
