75c10fe28928125e1c946905eb3495b575b0deb9
Jokerbm <37797569+Jokerbm@users.noreply.github.com>
2020-08-21 19:33:07 +0700

    optimize and will check

--- functions/api/controller/testController.js
+++ functions/api/controller/testController.js
@@ -1,28 +1,28 @@
 //require zone
-const compression = require('compression')
-const path = require('path');
-const cors = require('cors');
-const express = require('express');
-const admin = require('../config/admin');
+const compression = require("compression");
+const path = require("path");
+const cors = require("cors");
+const express = require("express");
+const admin = require("../config/admin");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const db = admin.firestore();
 const facebookStrategy = require("passport-facebook").Strategy;
 const firestore = admin.firestore();
-const { v4: uuidv4 } = require('uuid');
-const { S_IFBLK } = require('constants');
+const { v4: uuidv4 } = require("uuid");
+const { S_IFBLK } = require("constants");
 const testController = express();
-const SECRef = db.collection('secertfromuser');
-const BOUNTYRef = db.collection('bountys');
-const SBOUNTYRef = db.collection('bountyscan');
-const SCANSRef = db.collection('scans');
-const USERSRef = db.collection('users');
-const LINKRef = db.collection('links');
-const ALLRef = db.collection('allstats');
+const SECRef = db.collection("secertfromuser");
+const BOUNTYRef = db.collection("bountys");
+const SBOUNTYRef = db.collection("bountyscan");
+const SCANSRef = db.collection("scans");
+const USERSRef = db.collection("users");
+const LINKRef = db.collection("links");
+const ALLRef = db.collection("allstats");
 const helmet = require("helmet");
-var bunyan = require('bunyan');
-const { doc } = require('prettier');
+var bunyan = require("bunyan");
+const { doc } = require("prettier");
 var log = bunyan.createLogger({ name: "myapp" });
 log.info("hi");
 // server setup
@@ -36,8 +36,8 @@ testController.use(passport.session());
 testController.use(cookieParser());
 testController.use(compression());
 testController.use(cors({ origin: true }));
-testController.set('views', path.join(__dirname, 'views'));
-testController.set('view engine', 'ejs');
+testController.set("views", path.join(__dirname, "views"));
+testController.set("view engine", "ejs");
 //authen use
 passport.use(
     new facebookStrategy(
@@ -69,16 +69,16 @@ passport.use(
                             newuser: 1,
                             count: 0,
                         })
-                        const newusers = ALLRef.doc('stat').get().then((newusers)=> {
-                            const newuse = ALLRef.doc('stat').set({alluser: newusers.data().alluser + 1}, { merge: true })
+                        const newusers = ALLRef.doc("stat").get().then((newusers)=> {
+                            const newuse = ALLRef.doc("stat").set({alluser: newusers.data().alluser + 1}, { merge: true })
                         })
                     }
                     const usersnapshots = await USERSRef.doc(profile.id).get().then((doc) => {
-                        const vis = ALLRef.doc('stat').get().then((vis)=> {
-                            const visit = ALLRef.doc('stat').set({allvisitor: vis.data().allvisitor + 1 }, { merge: true })
-                        })
+                        const vis = ALLRef.doc("stat").get().then((vis)=> {
+                            const visit = ALLRef.doc("stat").set({allvisitor: vis.data().allvisitor + 1 }, { merge: true });
+                        });
                         log.info("user found");
-                        log.info(doc.data().name + " is logged in")
+                        log.info(doc.data().name + " is logged in");
                         return done(null, doc.data());
                     })
                 }
@@ -92,7 +92,7 @@ passport.use(
 passport.serializeUser(function (user, done) { done(null, user.id) }); //เก็บ id ไว้ใน session
 // used to deserialize the user //นำ id ที่เก็บไว้ใน session เรียกกลับมาใช้
 passport.deserializeUser(async function (id, done) {
-    const userdeserialize = await USERSRef.where('id', '==', id).get();
+    const userdeserialize = await USERSRef.where("id", "==", id).get();
     userdeserialize.forEach(doc => { done(null, doc.data()) });
 });
 
@@ -100,72 +100,72 @@ passport.deserializeUser(async function (id, done) {
 testController.get("/genqrcode", isLoggedIn, async function (req, res) {
     try {
         log.info("genQR: " + req.user.uid);
-        const genqrsnapshot = await LINKRef.where('uid', '==', req.user.uid).get();
+        const genqrsnapshot = await LINKRef.where("uid", "==", req.user.uid).get();
         const name = uuidv4();
         const data = { link: name, uid: req.user.uid, time: 10 };
         if (genqrsnapshot.empty) {
             log.info("create qr " + req.user.uid)
-            const newDoc = await db.collection('links').doc(req.user.uid).set(data)
+            const newDoc = await db.collection("links").doc(req.user.uid).set(data)
                 .then(() => {
                     res.status(200).send({
-                        'statusCode': '201',
-                        'statusText': 'Created',
-                        'error': false,
-                        'message': 'Successfully generated qr code',
-                        'qrcode': 'http://localhost:5001/itfreshy2020/us-central1/test/qrcode/' + name
+                        "statusCode": "201",
+                        "statusText": "Created",
+                        "error": false,
+                        "message": "Successfully generated qr code",
+                        "qrcode": "http://localhost:5001/itfreshy2020/us-central1/test/qrcode/" + name
                     });
                 }).catch((err) => {
                     res.status(400).send({
-                        'statusCode': '400',
-                        'statusText': 'Bad Request',
-                        'error': true,
-                        'message': 'QR code generate fail.'
+                        "statusCode": "400",
+                        "statusText": "Bad Request",
+                        "error": true,
+                        "message": "QR code generate fail."
                     })
                 })
         }
         else {
             genqrsnapshot.forEach(doc => {
                 if (doc.data().time <= 0) {
-                    log.info('Delete qr ' + req.user.uid)
-                    const qrDel = db.collection('links').doc(doc.id).delete()
+                    log.info("Delete qr " + req.user.uid)
+                    const qrDel = db.collection("links").doc(doc.id).delete()
                         .then(() => {
                             log.info(name + req.user.uid);
-                            const newDoc = db.collection('links').doc(req.user.uid).set(data).then(() => {
+                            const newDoc = db.collection("links").doc(req.user.uid).set(data).then(() => {
                                 res.status(200).send({
-                                    'statusCode': '201',
-                                    'statusText': 'Created',
-                                    'error': false,
-                                    'message': 'Successfully generated new qr code',
-                                    'qrcode': 'http://localhost:5001/itfreshy2020/us-central1/test/qrcode/' + name
+                                    "statusCode": "201",
+                                    "statusText": "Created",
+                                    "error": false,
+                                    "message": "Successfully generated new qr code",
+                                    "qrcode": "http://localhost:5001/itfreshy2020/us-central1/test/qrcode/" + name
                                 });
                             }).catch((err) => {
                                 res.status(400).send({
-                                    'statusCode': '400',
-                                    'statusText': 'Bad Request',
-                                    'error': true,
-                                    'message': 'QR code generate fail.'
+                                    "statusCode": "400",
+                                    "statusText": "Bad Request",
+                                    "error": true,
+                                    "message": "QR code generate fail."
                                 });
                             });
                         });
                 }
                 else {
-                    log.info('normal qR' + req.user.uid);
+                    log.info("normal qR" + req.user.uid);
                     res.status(200).send({
-                        'statusCode': '200',
-                        'statusText': 'Request Success',
-                        'error': false,
-                        'message': 'Successfully request qr',
-                        'qrcode': 'http://localhost:5001/itfreshy2020/us-central1/test/qrcode/' + doc.data().link
+                        "statusCode": "200",
+                        "statusText": "Request Success",
+                        "error": false,
+                        "message": "Successfully request qr",
+                        "qrcode": "http://localhost:5001/itfreshy2020/us-central1/test/qrcode/" + doc.data().link
                     });
                 }
             })
         };
     } catch (err) {
         res.status(500).send({
-            statusCode: '500',
-            statusText: 'Internal Server Error',
+            statusCode: "500",
+            statusText: "Internal Server Error",
             error: true,
-            message: 'Internal Server Error'
+            message: "Internal Server Error"
         });
     }
 });
@@ -174,7 +174,7 @@ testController.get("/qrcode/:id", isLoggedIn, async function (req, res) {
     let boun
     let usersa
     const useruid = req.user.uid
-    const linkRef = LINKRef.where('link', '==', req.params.id).get().then((linkRef, req) => {
+    const linkRef = LINKRef.where("link", "==", req.params.id).get().then((linkRef, req) => {
         if (linkRef.empty) {
             log.info(req.user.uid + " หา " + req.params.id + " ไม่เจอ ");
             res.send("ลิ้งคเสีย")
@@ -281,37 +281,37 @@ testController.route("/facebook/callback").get(
 
 testController.get("/api/user", isLoggedIn, (req, res) => {
     try {
-        log.info('---------->api/user');
+        log.info("---------->api/user");
         if (isLoggedIn) {
             res.status(200).json(req.user);
         } else {
             res.status(400).send({
-                'statusCode': '400',
-                'statusText': 'Bad Request',
-                'error': true,
-                'message': 'Not logged in.'
+                "statusCode": "400",
+                "statusText": "Bad Request",
+                "error": true,
+                "message": "Not logged in."
             });
         }
     } catch (err) {
         res.status(500).send({
-            statusCode: '500',
-            statusText: 'Internal Server Error',
+            statusCode: "500",
+            statusText: "Internal Server Error",
             error: true,
-            message: 'Internal Server Error'
+            message: "Internal Server Error"
         });
     }
 });
 
 testController.get("/", (req, res) => {
     try {
-        log.info('----------->Index')
+        log.info("----------->Index")
         res.status(200).render("index");
     } catch (err) {
         res.status(500).send({
-            statusCode: '500',
-            statusText: 'Internal Server Error',
+            statusCode: "500",
+            statusText: "Internal Server Error",
             error: true,
-            message: 'Internal Server Error'
+            message: "Internal Server Error"
         });
     }
 });
@@ -319,7 +319,7 @@ testController.get("/", (req, res) => {
 testController.get("/ryutools/:collection/:docname", isAdmin, async (req, res) => {
     log.info("king querty doc " + req.params.collection + " doc name " + req.params.docname)
     const all = await db.collection(req.params.collection).doc(req.params.docname).get();
-    // const all = await db.collection('bountys').doc('bounty').get();
+    // const all = await db.collection("bountys").doc("bounty").get();
     if (!all.exists) {
         res.send("cannot find")
         return "หาไม่เจอ"
@@ -328,20 +328,20 @@ testController.get("/ryutools/:collection/:docname", isAdmin, async (req, res) =
         res.send(all.data())
     }
     // try {
-    //     log.info('----------->Start');
+    //     log.info("----------->Start");
     //     //res.send("start")
     //     res.status(200).send({
-    //         'statusCode': '200',
-    //         'statusText': 'Request Success',
-    //         'error': false,
-    //         'message': 'Start'
+    //         "statusCode": "200",
+    //         "statusText": "Request Success",
+    //         "error": false,
+    //         "message": "Start"
     //     });
     // } catch (err) {
     //     res.status(500).send({
-    //         statusCode: '500',
-    //         statusText: 'Internal Server Error',
+    //         statusCode: "500",
+    //         statusText: "Internal Server Error",
     //         error: true,
-    //         message: 'Internal Server Error'
+    //         message: "Internal Server Error"
     //     });
     // }
 });
@@ -353,15 +353,15 @@ testController.get("/roletest", isAdmin ,async (req, res) => {
 
 testController.get("/logout", (req, res) => {
     try {
-        log.info('----------> Logout')
+        log.info("----------> Logout")
         req.logout();
         res.redirect("/");
     } catch (err) {
         res.status(500).send({
-            statusCode: '500',
-            statusText: 'Internal Server Error',
+            statusCode: "500",
+            statusText: "Internal Server Error",
             error: true,
-            message: 'Internal Server Error'
+            message: "Internal Server Error"
         });
     }
 });
@@ -376,19 +376,19 @@ function isLoggedIn(req, res, next) {
     try {
         // if user is authenticated in the session, carry on
         if (req.isAuthenticated()) {
-            log.info('---------->isOn');
+            log.info("---------->isOn");
             return next();
         }
-        else {    // if they aren't redirect them to the home page
-            log.info('----------->isOut');
+        else {    // if they aren"t redirect them to the home page
+            log.info("----------->isOut");
             res.redirect("/");
         }
     } catch (err) {
         res.status(500).send({
-            statusCode: '500',
-            statusText: 'Internal Server Error',
+            statusCode: "500",
+            statusText: "Internal Server Error",
             error: true,
-            message: 'Internal Server Error'
+            message: "Internal Server Error"
         });
     }
 }
@@ -396,8 +396,8 @@ function isLoggedIn(req, res, next) {
 function isAdmin(req, res, next) {
     try {
         if (req.isAuthenticated()) {
-            if (req.user.role == 'king') {
-                log.info('king ' + req.user.name +" use")
+            if (req.user.role == "king") {
+                log.info("king " + req.user.name +" use")
                 return next();
             }
             else {
@@ -412,10 +412,10 @@ function isAdmin(req, res, next) {
         }
     } catch (err) {
         res.status(500).send({
-            statusCode: '500',
-            statusText: 'Internal Server Error',
+            statusCode: "500",
+            statusText: "Internal Server Error",
             error: true,
-            message: 'Internal Server Error'
+            message: "Internal Server Error"
         });
     }
 }
