b1c0411d6227ceb772399d8514d3db0dbd521fdd
Pongsakorn Prawanna <newbeerstarsmcgamer@gmail.com>
2020-09-02 16:51:02 +0700

    Update Dashboard

--- functions/api/controller/dashboard.js
+++ functions/api/controller/dashboard.js
@@ -13,26 +13,29 @@ dashboardController.use(cors({ origin: true }));
 dashboardController.set("views", path.join(__dirname, "views"));
 dashboardController.set("view engine", "ejs");
 
+// var listData;
+
 dashboardController.get('/test', async (req, res) => {
     try{
         let userRef = firestore.collection('allstats').doc('stat');
         let snap = await userRef.get();
 
-        // res.render('dashboard', {
-        //     visit: snap.data().allvisitor,
-        //     user: snap.data().alluser,
-        //     gen: snap.data().allgenerate,
-        //     bounty: snap.data().allbounty,
-        //     scan: snap.data().allscan
-        // });
+        res.render('dashboard', {
+            visit: snap.data().allvisitor,
+            user: snap.data().alluser,
+            gen: snap.data().allgenerate,
+            bounty: snap.data().allbounty,
+            scan: snap.data().allscan
+        });
 
         const observer = firestore.collection('allstats').onSnapshot(querySnapshot => {
             querySnapshot.docChanges().forEach(change => {
             var data = change.doc.data();
-            res.render('dashboard', {user:data});
+            // res.render('dashboard', {user:data});
+            // changeData();
+            // listData = data;
             console.log(data);
-            res.end();
-            
+            // res.end();
             });
     });
     }catch(e){
@@ -43,8 +46,6 @@ dashboardController.get('/test', async (req, res) => {
             'message': 'FUCTION NOT FOUND'
         });
     }
-
 })
-
 
 module.exports = dashboardController;
